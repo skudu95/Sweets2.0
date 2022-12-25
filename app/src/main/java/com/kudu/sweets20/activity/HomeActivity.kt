b85cadc11777f1c04b57355c5f9a78ea19700c91
skudu95 <poko.sk.tolu@gmail.com>
2022-12-25 14:46:54 +0600

    token update commit check

--- app/src/main/java/com/kudu/sweets20/activity/HomeActivity.kt
+++ app/src/main/java/com/kudu/sweets20/activity/HomeActivity.kt
@@ -3,7 +3,11 @@ package com.kudu.sweets20.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.kudu.common.model.Categories
@@ -20,11 +24,55 @@ class HomeActivity : AppCompatActivity() {
     private val mFireStore = FirebaseFirestore.getInstance()
     private val productList: ArrayList<Products> = ArrayList()
     private val isFavourite: Boolean = false
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setUpActionBar()
+        /* toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
+         binding.root.addDrawerListener(toggle)
+         toggle.syncState()
+         supportActionBar?.setDisplayHomeAsUpEnabled(true)*/
+
+        //drawer
+        binding.btnMenu.setOnClickListener {
+            toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
+            binding.root.addDrawerListener(toggle)
+            toggle.syncState()
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            binding.root.openDrawer(GravityCompat.START)
+            Toast.makeText(this, "Menu Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        //side nav
+        binding.sideNav.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.side_nav_addresses -> {
+                    Toast.makeText(this, "Address Clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.side_nav_favourites -> {
+                    Toast.makeText(this, "Favourites Clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.side_nav_logout -> {
+                    Toast.makeText(this, "Logout Clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.side_nav_orders -> {
+                    Toast.makeText(this, "Orders Clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.side_nav_profile -> {
+                    Toast.makeText(this, "Profile Clicked", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                }
+                R.id.side_nav_voucher -> {
+                    Toast.makeText(this, "Voucher Clicked", Toast.LENGTH_SHORT).show()
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
+
 
         val categoryList: ArrayList<Categories> = ArrayList()
 
@@ -34,20 +82,20 @@ class HomeActivity : AppCompatActivity() {
         categoryList.add(Categories("Pizza", resources.getDrawable(R.drawable.pizza)))
         categoryList.add(Categories("Burgers", resources.getDrawable(R.drawable.burger)))
 
-//category recyclerview
+        //category recyclerview
         binding.rvCategories.setHasFixedSize(true)
         binding.rvCategories.setItemViewCacheSize(20)
         binding.rvCategories.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         binding.rvCategories.adapter = CategoriesListViewAdapter(this, categoryList)
 
-//popular item list
+        //popular item list
         binding.rvPopulars.setHasFixedSize(true)
         binding.rvPopulars.setItemViewCacheSize(20)
         binding.rvPopulars.layoutManager = LinearLayoutManager(this)
         binding.rvPopulars.adapter = PopularFoodListViewAdapter(this, productList)
 
-//order now button
+        //order now button
         binding.btnOrderNow.setOnClickListener {
             startActivity(Intent(this, CartActivity::class.java))
         }
@@ -56,6 +104,18 @@ class HomeActivity : AppCompatActivity() {
         binding.btnProfile.setOnClickListener {
             startActivity(Intent(this, ProfileActivity::class.java))
         }
+    }
+
+    //setting up actionbar with back button
+    private fun setUpActionBar() {
+        setSupportActionBar(binding.toolbarHomeActivity)
+
+        val actionbar = supportActionBar
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true)
+            actionbar.setHomeAsUpIndicator(R.drawable.back)
+        }
+//        binding.toolbarHomeActivity.setNavigationOnClickListener { onBackPressed() }
     }
 
     private fun getProductList() {
@@ -83,5 +143,12 @@ class HomeActivity : AppCompatActivity() {
             productList.clear()
             getProductList()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+
+        return super.onOptionsItemSelected(item)
     }
 }
